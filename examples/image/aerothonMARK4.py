d4b61b3d4921039ec0e5c0ba589a911b58a123bb
Anurag Shekhar <114636450+anurag2235@users.noreply.github.com>
2022-11-17 21:12:50 +0530

    Update aerothonMARK4.py

--- examples/image/aerothonMARK4.py
+++ examples/image/aerothonMARK4.py
@@ -10,7 +10,7 @@ from turtle import goto
 from dronekit import connect, VehicleMode, LocationGlobalRelative,LocationGlobal,Command
 from pymavlink import mavutil
 import math
-from classify import eye
+from functions import geta,getb,getc
 import numpy as np     
 import argparse
 parser = argparse.ArgumentParser(description='Commands vehicle using vehicle.simple_goto.')
@@ -29,26 +29,21 @@ def arm_and_takeoff(aTargetAltitude):
     """
     Arms vehicle and fly to aTargetAltitude.
     """
-
     print("Basic pre-arm checks")
     # Don't try to arm until autopilot is ready
     while not vehicle.is_armable:
         print(" Waiting for vehicle to initialise...")
         time.sleep(1)
-
     print("Arming motors")
     # Copter should arm in GUIDED mode
     vehicle.mode = VehicleMode("GUIDED")
     vehicle.armed = True
-
     # Confirm vehicle armed before attempting to take off
     while not vehicle.armed:
         print(" Waiting for arming...")
         time.sleep(1)
-
     print("Taking off!")
     vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
-
     # Wait until the vehicle reaches a safe height before processing the goto
     #  (otherwise the command after Vehicle.simple_takeoff will execute
     #   immediately).
@@ -71,8 +66,8 @@ def send_body_ned_velocity1(velocity_x, velocity_y, velocity_z, duration=0):
         0, 0, 0, # x, y, z acceleration
         0, 0)
     for x in range(0,duration):
-         a,b,c=eye()
-         if b>230:
+         b=getb()
+         if b>110:
             break;
          else:     
           vehicle.send_mavlink(msg)
@@ -134,24 +129,34 @@ def drop_parcel():
     # send command to vehicle
     vehicle.send_mavlink(msg)
     print("parcel dropped...")
-def distance():
+def cur_loc():
         f=vehicle.location.global_relative_frame.lat
         r=vehicle.location.global_relative_frame.lon
         return (f,r)
-#FOR PRECISION HOVERING
+def dis(a,b):
+    d=math.sqrt((a-120)*(a-120)+(b-120)*(b-120))
+    return d        
+#FOR PRECISION HOVERING AND PAYLOAD DROP
 # FLIGHT COMMANDS
 arm_and_takeoff(30)
+#################################################################################################
+#################################################################################################
 point1 = LocationGlobalRelative(28.3677924,77.3163593, 30)
 point2 = LocationGlobalRelative(28.3678502,77.3164827, 30)
 point3 = LocationGlobalRelative(28.3692556,77.3159194, 30)
+
+
+
+#################################################################################################
+#################################################################################################
 vehicle.simple_goto(point1,groundspeed=5)
 print("going to first waypoint")
 pointa=(28.3677924,77.3163593)
-pointb=distance()
+pointb=cur_loc()
 while (GD(pointa,pointb))*1000>1:
-    a,b,c=eye()
+    c=getc()
     if c==0:
-        pointb=distance()
+        pointb=cur_loc()
         continue
     else:
         d=vehicle.location.global_relative_frame.lat
@@ -161,24 +166,26 @@ while (GD(pointa,pointb))*1000>1:
         current=LocationGlobalRelative(d,e,30)
         vehicle.simple_goto(current,groundspeed=1)
         time.sleep(15)
+        print("aligning")
         condition_yaw(180,True)
         time.sleep(15)
-        a,b,c=eye()
-        d=math.sqrt((a-320)*(a-320)+(b-240)*(b-240))
-        if a<320 and b<240:
-               theta=math.degrees(math.asin((320-a)/d))
-        elif a>320 and b<240:
-               theta=360-math.degrees(math.asin((a-320)/d))
-        elif a<320 and b>240:
-               theta=90+math.degrees(math.asin((b-240)/d))
-        elif a>320 and b>240:
-               theta=180+math.degrees(math.asin((a-320)/d))              
+        b=getb()
+        a=geta()
+        d=dis(a,b)
+        if a<120 and b<120:
+               theta=math.degrees(math.asin((120-a)/d))
+        elif a>120 and b<120:
+               theta=360-math.degrees(math.asin((a-120)/d))
+        elif a<120 and b>120:
+               theta=90+math.degrees(math.asin((b-120)/d))
+        elif a>120 and b>120:
+               theta=180+math.degrees(math.asin((a-120)/d))              
         condition_yaw(theta,True)  
         print("docking") 
         time.sleep(10)
-        a,b,c=eye()
+        c=getc()
         if(c==1):
-          send_body_ned_velocity1(0.5,0,0,100)
+          send_body_ned_velocity1(0.5,0,0,15)
           time.sleep(10)
           send_body_ned_velocity2(0,0,0.5,100)
           time.sleep(10)
@@ -201,14 +208,16 @@ while (GD(pointa,pointb))*1000>1:
             print("returning to launch site")
             vehicle.mode=VehicleMode("RTL")
             exit()
+#########################################################################################################
+#########################################################################################################            
 print("going to second waypoint")        
 vehicle.simple_goto(point2,groundspeed=50)
 pointa=(28.3678502,77.3164827)
-pointb=distance()
+pointb=cur_loc()
 while (GD(pointa,pointb))*1000>1:
-    a,b,c=eye()
+    c=getc()
     if c==0:
-        pointb=distance()
+        pointb=cur_loc()
         continue
     else:
         d=vehicle.location.global_relative_frame.lat
@@ -220,20 +229,21 @@ while (GD(pointa,pointb))*1000>1:
         time.sleep(15)
         condition_yaw(180,True)
         time.sleep(15)
-        a,b,c=eye()
-        d=math.sqrt((a-320)*(a-320)+(b-240)*(b-240))
-        if a<320 and b<240:
-               theta=math.degrees(math.asin((320-a)/d))
-        elif a>320 and b<240:
-               theta=360-math.degrees(math.asin((a-320)/d))
-        elif a<320 and b>240:
-               theta=90+math.degrees(math.asin((b-240)/d))
-        elif a>320 and b>240:
-               theta=180+math.degrees(math.asin((a-320)/d))            
+        a=geta()
+        b=getb()
+        d=dis(a,b)
+        if a<120 and b<120:
+               theta=math.degrees(math.asin((120-a)/d))
+        elif a>120 and b<120:
+               theta=360-math.degrees(math.asin((a-120)/d))
+        elif a<120 and b>120:
+               theta=90+math.degrees(math.asin((b-120)/d))
+        elif a>120 and b>120:
+               theta=180+math.degrees(math.asin((a-120)/d))           
         condition_yaw(theta,True)  
         print("docking") 
         time.sleep(10)
-        a,b,c=eye()
+        c=getc()
         if(c==1):
           send_body_ned_velocity1(0.5,0,0,100)
           time.sleep(10)
@@ -258,15 +268,16 @@ while (GD(pointa,pointb))*1000>1:
             print("returning to launch site")
             vehicle.mode=VehicleMode("RTL")
             exit()
-
+############################################################################################################
+############################################################################################################
 print("going to third waypoint")        
 vehicle.simple_goto(point3,groundspeed=50)
 pointa=(28.3692556,77.3159194)
-pointb=distance()
+pointb=cur_loc()
 while (GD(pointa,pointb))*1000>1:
-    a,b,c=eye()
+    c=getc()
     if c==0:
-        pointb=distance()
+        pointb=cur_loc()
         continue
     else:
         d=vehicle.location.global_relative_frame.lat
@@ -278,20 +289,21 @@ while (GD(pointa,pointb))*1000>1:
         time.sleep(15)
         condition_yaw(180,True)
         time.sleep(15)
-        a,b,c=eye()
-        d=math.sqrt((a-320)*(a-320)+(b-240)*(b-240))
-        if a<320 and b<240:
-               theta=math.degrees(math.asin((320-a)/d))
-        elif a>320 and b<240:
-               theta=360-math.degrees(math.asin((a-320)/d))
-        elif a<320 and b>240:
-               theta=90+math.degrees(math.asin((b-240)/d))
-        elif a>320 and b>240:
-               theta=180+math.degrees(math.asin((a-320)/d))            
+        a=geta()
+        b=getb()
+        d=dis(a,b)
+        if a<120 and b<120:
+               theta=math.degrees(math.asin((120-a)/d))
+        elif a>120 and b<120:
+               theta=360-math.degrees(math.asin((a-120)/d))
+        elif a<120 and b>120:
+               theta=90+math.degrees(math.asin((b-120)/d))
+        elif a>120 and b>120:
+               theta=180+math.degrees(math.asin((a-120)/d))            
         condition_yaw(theta,True)  
         print("docking") 
         time.sleep(10)
-        a,b,c=eye()
+        c=getc()
         if(c==1):
           send_body_ned_velocity1(0.5,0,0,100)
           time.sleep(10)
@@ -316,8 +328,10 @@ while (GD(pointa,pointb))*1000>1:
             print("returning to launch site")
             vehicle.mode=VehicleMode("RTL")
             exit()
-
-time.sleep(2)
+########################################################################################################
+########################################################################################################            
+time.sleep(5)
 print("returning to launch site")
+print("MISSION COMPLETED")
 vehicle.mode=VehicleMode("RTL")
 exit()        
